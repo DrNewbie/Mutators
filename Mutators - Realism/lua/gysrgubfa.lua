cb82999b1f031bb8edb31bbdfe916d6b6b8927f9
Dr_Newbie <tf2baidonation@gmail.com>
2016-10-16 03:36:42 +0800

    2

--- Mutators - Realism/lua/gysrgubfa.lua
+++ Mutators - Realism/lua/gysrgubfa.lua
@@ -1,7 +1,15 @@
+_G._tmp_mutator_saving = _G._tmp_mutator_saving or {}
+
 Hooks:PostHook(HUDManager, "set_enabled", "MutatorRealismBlockEnable", function(hudd, ...)
-	hudd:set_disabled()
+	if MutatorRealism and _tmp_mutator_saving.MutatorRealism then
+		hudd:set_disabled()
+	end
 end )
 
 Hooks:PostHook(HUDManager, "init", "MutatorRealismEnable", function(hudd, ...)
-	hudd:set_disabled()
+	if MutatorRealism and _tmp_mutator_saving.MutatorRealism then
+		hudd:set_disabled()
+	else
+		hudd:set_enabled()
+	end
 end )
--- Mutators - Realism/lua/mutatorsmanager.lua
+++ Mutators - Realism/lua/mutatorsmanager.lua
@@ -1,3 +1,35 @@
 Hooks:PostHook(MutatorsManager, "init", "MutatorRealismNew", function(mm, ...)
 	table.insert(mm._mutators, MutatorRealism:new(mm))
 end )
+
+_G._tmp_mutator_saving = _G._tmp_mutator_saving or {}
+
+local _file = io.open(SavePath .. "tmp_mutator_saving.txt", "r")
+if _file then
+	_tmp_mutator_saving = json.decode(_file:read("*all"))
+	_file:close()
+end
+
+function MutatorsManager:set_enabled(mutator, enabled)
+	if enabled == nil then
+		enabled = true
+	end
+	if type(mutator) == "string" then
+		mutator = self:get_mutator_from_id(mutator)
+	end
+	if not mutator then
+		return false
+	end
+	if enabled and self:can_enable_mutator(mutator) then
+		mutator:set_enabled(enabled)	
+		_tmp_mutator_saving[tostring(mutator:id())] = enabled
+	else
+		mutator:set_enabled(false)
+		_tmp_mutator_saving[tostring(mutator:id())] = false
+	end	
+	local _file = io.open(SavePath .. "tmp_mutator_saving.txt", "w+")
+	if _file then
+		_file:write(json.encode(_tmp_mutator_saving))
+		_file:close()
+	end
+end
