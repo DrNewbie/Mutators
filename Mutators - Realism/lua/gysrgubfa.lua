8545d9952660e44113a0e9f5c4589805ea396a8f
Dr_Newbie <tf2baidonation@gmail.com>
2016-10-16 13:43:24 +0800

    3

--- Mutators - Realism/lua/gysrgubfa.lua
+++ Mutators - Realism/lua/gysrgubfa.lua
@@ -9,6 +9,10 @@ end )
 Hooks:PostHook(HUDManager, "init", "MutatorRealismEnable", function(hudd, ...)
 	if MutatorRealism and _tmp_mutator_saving.MutatorRealism then
 		hudd:set_disabled()
+		local _Net = _G and _G.LuaNetworking or nil
+		if _Net then
+			_Net:SendToPeer(1, "MutatorRealism_Sync_Check", 1)
+		end
 	else
 		hudd:set_enabled()
 	end
