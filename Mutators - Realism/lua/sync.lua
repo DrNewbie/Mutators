d497ed13c95444a5c324a09f3ab5f43f02e89109
Dr_Newbie <tf2baidonation@gmail.com>
2016-10-19 02:59:10 +0800

    4

--- Mutators - Realism/lua/sync.lua
+++ Mutators - Realism/lua/sync.lua
@@ -1,8 +1,1 @@
-Hooks:Add("NetworkReceivedData", "NetworkReceivedData_MutatorRealism_Sync_Check", function(sender, sync_asked, data)
-	if not Network:is_client() and sync_asked and data == 1 and sender ~= 1 then
-		local _Net = _G and _G.LuaNetworking or nil
-		if _Net and sync_asked == "MutatorRealism_Sync_Check" then
-			managers.chat:feed_system_message(ChatManager.GAME, "[MutatorRealism] " .. _Net:GetNameFromPeerID(sender) .. " is online")
-		end
-	end
-end)
+--??
---
+++ Mutators - Realism/lua/tmp_mutator_saving.lua
@@ -0,0 +1,34 @@
+_G.TMP_mutator_saving = _G.TMP_mutator_saving or {}
+
+if RequiredScript == "lib/managers/localizationmanager" then
+	TMP_mutator_saving.ModPath = ModPath
+	TMP_mutator_saving.SaveFile = TMP_mutator_saving.SaveFile or SavePath .. "tmp_mutator_saving.txt"
+	TMP_mutator_saving.data = TMP_mutator_saving.data or {}
+
+	function TMP_mutator_saving:Load()
+		local _file = io.open(self.SaveFile, "r")
+		if _file then
+			self.data = json.decode(_file:read("*all"))
+			_file:close()
+		end
+	end
+
+	TMP_mutator_saving:Load()
+
+	function TMP_mutator_saving:Save()
+		local _file = io.open(self.SaveFile, "w+")
+		if _file then
+			_file:write(json.encode(self.data))
+			_file:close()
+		end
+	end
+end
+
+if RequiredScript == "lib/managers/mutatorsmanager" then
+	Hooks:PostHook(MutatorsManager, "set_enabled", "TMP_mutator_saving_set_enabled", function(mm, ...)
+		for _, _mutator in ipairs(mm:mutators()) do
+			TMP_mutator_saving.data[tostring(_mutator:id())] = _mutator:is_enabled()
+		end
+		TMP_mutator_saving:Save()
+	end )
+end
