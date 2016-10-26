431e3af90d68cbd6559e27d62a5197967e019b24
Dr_Newbie <tf2baidonation@gmail.com>
2016-10-27 01:19:46 +0800

    BFFs Plus ; 3 ; Allow to Change Special and Normal

--- Mutators - BFFs Plus/lua/menumanager.lua
+++ Mutators - BFFs Plus/lua/menumanager.lua
@@ -1,6 +1,6 @@
 if managers.mutators:is_mutator_active(MutatorMediDozer) then
 	local _mm = managers.mutators:get_mutator(MutatorMediDozer)
 	if _mm and _mm:get_override_enemy() then
-		Announcer:AddHostMod("BFFs Plus, Special enemies are replaced with others, normal enemies are replaced with Medic")
+		Announcer:AddHostMod("BFFs Plus, Special enemies are replaced with one, normal enemies are replaced with other")
 	end
 end
--- Mutators - BFFs Plus/lua/mutatorenemyreplacer.lua
+++ Mutators - BFFs Plus/lua/mutatorenemyreplacer.lua
@@ -1,8 +1,9 @@
-Hooks:Add("LocalizationManagerPostInit", "SafeHousePlus_loc", function(loc)
+Hooks:Add("LocalizationManagerPostInit", "MutatorMediDozer_loc", function(loc)
 	LocalizationManager:add_localized_strings({
 		["mutator_medidozer"] = "BFFs Plus",
-		["mutator_medidozer_desc"] = "Special enemies are replaced with others, normal enemies are replaced with Medic",
+		["mutator_medidozer_desc"] = "Special enemies are replaced with one, normal enemies are replaced with other",
 		["mutator_specials_override_phalanx"] = "Phalanx",
+		["mutator_specials_override_select_normal"] = "Normal Unit",
 	})
 end)
 
@@ -20,41 +21,64 @@ MutatorMediDozer.icon_coords = {8, 1}
 
 function MutatorMediDozer:name(lobby_data)
 	local name = MutatorMediDozer.super.name(self)
-	if self:_mutate_name("bffs_override_enemy") then
-		return string.format("%s - %s", name, managers.localization:text("mutator_specials_override_" .. tostring(self:value("bffs_override_enemy"))))
+	if self:_mutate_name("bffs_override_enemy_special") then
+		local _special = managers.localization:text("mutator_specials_override_" .. tostring(self:value("bffs_override_enemy_special")))
+		local _normal = managers.localization:text("mutator_specials_override_" .. tostring(self:value("bffs_override_enemy_normal")))
+		return string.format("%s - %s + %s", name, _special, _normal)
 	else
 		return name
 	end
 end
 
 function MutatorMediDozer:register_values(mutator_manager)
-	self:register_value("bffs_override_enemy", self:default_override_enemy(), "oe")
+	self:register_value("bffs_override_enemy_special", self:default_override_enemy_special(), "oe")
+	self:register_value("bffs_override_enemy_normal", self:default_override_enemy_normal(), "oe")
 end
 
-function MutatorMediDozer:get_override_enemy()
-	return self:value("bffs_override_enemy")
+function MutatorMediDozer:get_override_enemy_special()
+	return self:value("bffs_override_enemy_special")
 end
 
-function MutatorMediDozer:_update_selected_enemy(item)
-	self:set_value("bffs_override_enemy", item:value())
+function MutatorMediDozer:get_override_enemy_normal()
+	return self:value("bffs_override_enemy_normal")
+end
+
+function MutatorMediDozer:_update_selected_enemy_special(item)
+	self:set_value("bffs_override_enemy_special", item:value())
+end
+
+function MutatorMediDozer:_update_selected_enemy_normal(item)
+	self:set_value("bffs_override_enemy_normal", item:value())
+end
+
+function MutatorMediDozer:default_override_enemy_special()
+	return "tank"
+end
+
+function MutatorMediDozer:default_override_enemy_normal()
+	return "medic"
 end
 
 function MutatorMediDozer:reset_to_default()
 	self:clear_values()
 	if self._node then
-		local slider = self._node:item("bffs_pluse_enemy_selector_choice")
+		local slider = self._node:item("bffs_pluse_enemy_selector_choice_special")
 		if slider then
-			slider:set_value(self:default_override_enemy())
+			slider:set_value(self:default_override_enemy_special())
+		end
+		slider = self._node:item("bffs_pluse_enemy_selector_choice_normal")
+		if slider then
+			slider:set_value(self:default_override_enemy_normal())
 		end
 	end
 end
 
 function MutatorMediDozer:setup_options_gui(node)
 	local params = {
-		name = "bffs_pluse_enemy_selector_choice",
+		name = "bffs_pluse_enemy_selector_choice_special",
 		text_id = "mutator_specials_override_select",
 		callback = "_update_mutator_value",
-		update_callback = callback(self, self, "_update_selected_enemy"),
+		update_callback = callback(self, self, "_update_selected_enemy_special"),
 		filter = true
 	}
 	local data_node = {
@@ -86,25 +110,50 @@ function MutatorMediDozer:setup_options_gui(node)
 		}
 	}
 	local new_item = node:create_item(data_node, params)
-	new_item:set_value(self:get_override_enemy())
+	new_item:set_value(self:get_override_enemy_special())
+	node:add_item(new_item)
+	self._node = node
+	
+	local params = {
+		name = "bffs_pluse_enemy_selector_choice_normal",
+		text_id = "mutator_specials_override_select_normal",
+		callback = "_update_mutator_value",
+		update_callback = callback(self, self, "_update_selected_enemy_normal"),
+		filter = true
+	}
+	local data_node = {
+		type = "MenuItemMultiChoice",
+		{
+			_meta = "option",
+			text_id = "mutator_specials_override_tank",
+			value = "tank"
+		},
+		{
+			_meta = "option",
+			text_id = "mutator_specials_override_taser",
+			value = "taser"
+		},
+		{
+			_meta = "option",
+			text_id = "mutator_specials_override_shield",
+			value = "shield"
+		},
+		{
+			_meta = "option",
+			text_id = "mutator_specials_override_spooc",
+			value = "spooc"
+		},
+		{
+			_meta = "option",
+			text_id = "mutator_specials_override_phalanx",
+			value = "phalanx"
+		}
+	}
+	local new_item = node:create_item(data_node, params)
+	new_item:set_value(self:get_override_enemy_normal())
 	node:add_item(new_item)
 	self._node = node
 	return new_item
-end
-
-function MutatorMediDozer:_get_unit_group_tank()
-end
-
-function MutatorMediDozer:_get_unit_group_taser()
-end
-
-function MutatorMediDozer:_get_unit_group_spooc()
-end
-
-function MutatorMediDozer:_get_unit_group_shield()
-end
-
-function MutatorMediDozer:_get_unit_group_medic()
 end
 
 local access_type_walk_only = {walk = true}
@@ -115,57 +164,35 @@ function MutatorMediDozer:modify_unit_categories(group_ai_tweak, difficulty_inde
 		taser = 0,
 		spooc = 0,
 		shield = 0,
-		medic = math.huge
+		medic = 0
 	}
-	if self:get_override_enemy() == "phalanx" then
+	local _special = self:get_override_enemy_special()
+	local _normal = self:get_override_enemy_normal()
+	if _special == "phalanx" then
 		group_ai_tweak.special_unit_spawn_limits.shield = math.huge
 	else
-		group_ai_tweak.special_unit_spawn_limits[self:get_override_enemy()] = math.huge
+		group_ai_tweak.special_unit_spawn_limits[_special] = math.huge
 	end
-	group_ai_tweak.unit_categories.medic_R870 = {
-		unit_types = {
-			america = {
-				Idstring("units/payday2/characters/ene_medic_m4/ene_medic_m4"),
-				Idstring("units/payday2/characters/ene_medic_m4/ene_medic_m4")
-			},
-			russia = {
-				Idstring("units/payday2/characters/ene_medic_m4/ene_medic_m4"),
-				Idstring("units/payday2/characters/ene_medic_m4/ene_medic_m4")
-			}
-		},
-		access = access_type_all,
-		special_type = "medic"
-	}
-	group_ai_tweak.unit_categories.medic_R870 = {
-		unit_types = {
-			america = {
-				Idstring("units/payday2/characters/ene_medic_r870/ene_medic_r870"),
-				Idstring("units/payday2/characters/ene_medic_r870/ene_medic_r870")
-			},
-			russia = {
-				Idstring("units/payday2/characters/ene_medic_r870/ene_medic_r870"),
-				Idstring("units/payday2/characters/ene_medic_r870/ene_medic_r870")
-			}
-		},
-		access = access_type_all,
-		special_type = "medic"
-	}
-	local _unit_categories_type = {
-		tank = "FBI_tank",
-		taser = "CS_tazer",
-		spooc = "spooc",
-		shield = "FBI_shield",
-		phalanx = "Phalanx_minion"
+	if _normal == "phalanx" then
+		group_ai_tweak.special_unit_spawn_limits.shield = math.huge
+	else
+		group_ai_tweak.special_unit_spawn_limits[_normal] = math.huge
+	end
+	local _unit_categories = {
+		tank = self:_get_unit_group_tank(difficulty_index),
+		taser = self:_get_unit_group_taser(difficulty_index),
+		spooc = self:_get_unit_group_spooc(difficulty_index),
+		shield = self:_get_unit_group_shield(difficulty_index),
+		medic = self:_get_unit_group_medic(difficulty_index),
+		phalanx = group_ai_tweak.unit_categories["Phalanx_minion"]
 	}
 	for group, units_data in pairs(group_ai_tweak.unit_categories) do
 		if units_data.special_type then
-			if units_data.special_type ~= self:get_override_enemy() and units_data.special_type ~= "medic" then
-				group_ai_tweak.unit_categories[group] = group_ai_tweak.unit_categories[_unit_categories_type[self:get_override_enemy()]]
+			if units_data.special_type ~= _special then
+				group_ai_tweak.unit_categories[group] = _unit_categories[_special]
 			end
-		elseif string.find(group, "r870") then
-			group_ai_tweak.unit_categories[group] = group_ai_tweak.unit_categories.medic_R870
 		else
-			group_ai_tweak.unit_categories[group] = group_ai_tweak.unit_categories.medic_M4
+			group_ai_tweak.unit_categories[group] = _unit_categories[_normal]
 		end
 	end
 end
