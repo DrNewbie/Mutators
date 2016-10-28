b535fdc4ad7b36300f1497b7ba58d0dd09bbc992
Dr_Newbie <tf2baidonation@gmail.com>
2016-10-28 19:54:08 +0800

    update 4 new saving

--- Mutators -  Quick-Fix/lua/mutatorsmanager.lua
+++
@@ -1,3 +0,0 @@
-Hooks:PostHook(MutatorsManager, "init", "MutatorQuickFix_Init", function(mm, ...)
-	table.insert(mm._mutators, MutatorQuickFix:new(mm))
-end )
--- Mutators - Zero Down/lua/teamaidamage.lua
+++ Mutators - Zero Down/lua/teamaidamage.lua
@@ -1,7 +1,5 @@
 _G.TMP_mutator_saving = _G.TMP_mutator_saving or {}
 
-TMP_mutator_saving.data = TMP_mutator_saving.data or {}
-
 Hooks:PostHook(TeamAIDamage, "init", "MutatorZeroDown_init_Init", function(aii, ...)
 	if TMP_mutator_saving:Is_This_Enable("MutatorZeroDown") then
 		aii._char_dmg_tweak.DOWNED_TIME = 0
