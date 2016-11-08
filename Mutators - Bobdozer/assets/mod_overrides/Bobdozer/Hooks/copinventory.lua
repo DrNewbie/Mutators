5fb929ffc1822d98a17c2e81dbe66b95a982cadd
Dr_Newbie <tf2baidonation@gmail.com>
2016-11-09 01:46:04 +0800

    Bobdozer ; with Dozer Mask

---
+++ Mutators - Bobdozer/assets/mod_overrides/Bobdozer/Hooks/copinventory.lua
@@ -0,0 +1,67 @@
+function CopInventory:preload_mask()
+	if self._unit:name():key() ~= Idstring("units/payday2/characters/civ_male_casual_1/civ_male_casual_1"):key() then
+		return
+	end
+	self._mask_visibility = true
+	self._mask_unit_name = "units/pd2_merchandise/masks/msk_bobblehead_dozer/msk_bobblehead_dozer"
+	managers.dyn_resource:load(Idstring("unit"), Idstring(self._mask_unit_name), managers.dyn_resource.DYN_RESOURCES_PACKAGE, callback(self, self, "clbk_mask_unit_loaded"))
+end
+
+function CopInventory:clbk_mask_unit_loaded(status, asset_type, asset_name)
+	self._mask_unit_loaded = status
+	self:_reset_mask_visibility()
+end
+
+function CopInventory:is_mask_unit_loaded()
+	return self._mask_unit_loaded
+end
+
+function CopInventory:_unload_mask()
+	if self._mask_unit_name then
+		managers.dyn_resource:unload(Idstring("unit"), Idstring(self._mask_unit_name), DynamicResourceManager.DYN_RESOURCES_PACKAGE, false)
+		self._mask_unit_name = nil
+	end
+end
+
+function CopInventory:_reset_mask_visibility()
+	self:set_mask_visibility(self._mask_visibility and true or false)
+end
+
+function CopInventory:pre_destroy(unit)
+	CopInventory.super.pre_destroy(self, unit)
+	self:_unload_mask()
+end
+
+function CopInventory:set_mask_visibility(state)
+	self._mask_visibility = state
+	if self._unit == managers.player:player_unit() then
+		return
+	end
+	local character_name = managers.criminals:character_name_by_unit(self._unit)
+	if character_name then
+		return
+	end
+	self._mask_visibility = state
+	if alive(self._mask_unit) then
+		if not state then
+			for _, linked_unit in ipairs(self._mask_unit:children()) do
+				linked_unit:unlink()
+				World:delete_unit(linked_unit)
+			end
+			self._mask_unit:unlink()
+			local name = self._mask_unit:name()
+			World:delete_unit(self._mask_unit)
+		end
+		return
+	end
+	if not state then
+		return
+	end
+	mask_unit_name = self._mask_unit_name
+	local mask_align = self._unit:get_object(Idstring("Head"))
+	local mask_unit = World:spawn_unit(Idstring(mask_unit_name), mask_align:position(), mask_align:rotation())
+	self._unit:link(mask_align:name(), mask_unit, mask_unit:orientation_object():name())
+	self._mask_unit = mask_unit
+	local backside = World:spawn_unit(Idstring("units/payday2/masks/msk_backside/msk_backside"), mask_align:position(), mask_align:rotation())
+	self._mask_unit:link(self._mask_unit:orientation_object():name(), backside, backside:orientation_object():name())
+end
--- Mutators - Bobdozer/assets/mod_overrides/Bobdozer/Hooks/copmovement.lua
+++ Mutators - Bobdozer/assets/mod_overrides/Bobdozer/Hooks/copmovement.lua
@@ -1,1 +1,5 @@
 CopMovement._action_variants["bobdozer"] = CopMovement._action_variants["tank"]
+
+Hooks:PostHook(CopMovement, "set_character_anim_variables", "Bobdozer_GetMask", function(cop, ...)
+	cop._unit:inventory():preload_mask()
+end)
