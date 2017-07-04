feb3a35160bacd78a65ac6c49a21b521b5c6e1f4
Dr_Newbie <tf2baidonation@gmail.com>
2017-07-04 23:50:51 +0800

    Bankcloaker - don't spawn me if not active

--- Mutators - Bankcloaker/assets/mod_overrides/Bankspook/Hooks/elementspawncivilian.lua
+++ Mutators - Bankcloaker/assets/mod_overrides/Bankspook/Hooks/elementspawncivilian.lua
@@ -3,28 +3,23 @@ ElementSpawnCivilian = ElementSpawnCivilian or class(CoreMissionScriptElement.Mi
 
 local _Bankspook_ElementSpawnCivilian_produce = ElementSpawnCivilian.produce
 
+_G.TMP_mutator_saving = _G.TMP_mutator_saving or {}
+
 function ElementSpawnCivilian:produce(...)
 	local _res = _Bankspook_ElementSpawnCivilian_produce(self, ...)
+	local _Is_Enable = TMP_mutator_saving:Is_This_Enable("MutatorBankspook", MutatorBankspook)
 	local _SP = {}
 	_SP[Idstring("units/payday2/characters/civ_male_bank_1/civ_male_bank_1"):key()] = true
 	_SP[Idstring("units/payday2/characters/civ_male_bank_2/civ_male_bank_2"):key()] = true
-	if _res and alive(_res) and _SP[_res:name():key()] then
-		local _team = "combatant"
-		local _team_id = tweak_data.levels:get_default_team_ID(_team)
-		_res:movement():set_team(managers.groupai:state():team_data(_team_id))		
-		managers.groupai:state():_clear_criminal_suspicion_data()
+	if _SP[self._enemy_name:key()] then
+		if _Is_Enable then
+			local _team = "combatant"
+			local _team_id = tweak_data.levels:get_default_team_ID(_team)
+			_res:movement():set_team(managers.groupai:state():team_data(_team_id))
+			managers.groupai:state():_clear_criminal_suspicion_data()
+		else
+			_res:set_slot(0)
+		end
 	end
 	return _res
-end
-
-_G.TMP_mutator_saving = _G.TMP_mutator_saving or {}
-
-if TMP_mutator_saving:Is_This_Enable("MutatorBankspook", MutatorBankspook) then
-	local MutatorBankspook_Assets = "assets/mod_overrides/Bankspook/Assets/units/payday2/characters/"
-	BeardLib:ReplaceScriptData(MutatorBankspook_Assets .. "civ_male_bank_1/civ_male_bank_1.unit", "xml", "units/payday2/characters/civ_male_bank_1/civ_male_bank_1", "unit", {custom = true})
-	BeardLib:ReplaceScriptData(MutatorBankspook_Assets .. "civ_male_bank_1/civ_male_bank_1_husk.unit", "xml", "units/payday2/characters/civ_male_bank_1/civ_male_bank_1_husk", "unit", {custom = true})
-	BeardLib:ReplaceScriptData(MutatorBankspook_Assets .. "civ_male_bank_1/civ_male_bank_1.object", "xml", "units/payday2/characters/civ_male_bank_1/civ_male_bank_1", "object", {custom = true})
-	BeardLib:ReplaceScriptData(MutatorBankspook_Assets .. "civ_male_bank_2/civ_male_bank_2.unit", "xml", "units/payday2/characters/civ_male_bank_2/civ_male_bank_2", "unit", {custom = true})
-	BeardLib:ReplaceScriptData(MutatorBankspook_Assets .. "civ_male_bank_2/civ_male_bank_2_husk.unit", "xml", "units/payday2/characters/civ_male_bank_2/civ_male_bank_2_husk", "unit", {custom = true})
-	BeardLib:ReplaceScriptData(MutatorBankspook_Assets .. "civ_male_bank_2/civ_male_bank_2.object", "xml", "units/payday2/characters/civ_male_bank_2/civ_male_bank_2", "object", {custom = true})
 end
